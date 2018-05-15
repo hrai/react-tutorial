addd376f8e15ec778e0bc65c392c8aa0e96c885d
hrai <rai.hangjit@gmail.com>
2018-05-15 13:56:13 +1000

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Games from "./Games.jsx";
+import "./index.css";
+
+ReactDOM.render(<Games />, document.getElementById("root"));
